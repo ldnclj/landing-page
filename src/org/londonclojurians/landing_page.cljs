04a77bc54bf3a67db6309611009ea53e10c985d1
John Practicalli Stevenson <john@jr0cket.co.uk>
2021-04-19 00:10:36 +0100

    landing-page: add reagent, update index page and included bulma
    reagent 1.0.0 has a slightly different API to version 0.8.0, which the
    fighwheel-template adds using the +reagent option.
    
    Moved the index.html file from the figwheel-main template to default.html
    
    Added latest bulma.io CSS framework version to new index.html file

--- src/org/londonclojurians/landing_page.cljs
+++ src/org/londonclojurians/landing_page.cljs
@@ -1,22 +1,72 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; London Clojurains Website
+;;
+;; A ClojureScript single page app with reagent
+;; Created with Clojure CLI tools and figwheel-main template
+;; https://ldnclj.github.io
+;;
+;; Author(s): John Stevenson
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
 (ns ^:figwheel-hooks org.londonclojurians.landing-page
   (:require
-   [goog.dom :as gdom]))
+   [goog.dom :as gdom]
+   [reagent.core :as reagent]
+   [reagent.dom :as dom]))
 
-(println "This text is printed from src/org/londonclojurians/landing_page.cljs. Go ahead and edit it and see reloading in action.")
 
-(defn multiply [a b] (* a b))
+;; simple debug statement for each build
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(println (js/Date.) "Reloading: src/org/londonclojurians/landing_page.cljs")
+
+
+;; Application State
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state
+  (reagent/atom {:text "London Clojurians"}))
+
+
+;; Website structure
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn main-page
+  "The layout of components on the main page."
+  []
+  [:div
+   [:h1 (:text @app-state)]])
+
+
+
+;; System configuration code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
 
+(defn mount [el]
+  (dom/render [main-page] el))
+
+
+(defn mount-app-element []
+  (when-let [el (get-app-element)]
+    (mount el)))
+
+;; conditionally start your application based on the presence of an "app" element
+;; this is particularly helpful for testing this ns without launching the app
+(mount-app-element)
 
 ;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
+  (mount-app-element)
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
--- test/org/londonclojurians/landing_page_test.cljs
+++ test/org/londonclojurians/landing_page_test.cljs
@@ -1,10 +1,4 @@
 (ns org.londonclojurians.landing-page-test
     (:require
      [cljs.test :refer-macros [deftest is testing]]
-     [org.londonclojurians.landing-page :refer [multiply]]))
-
-(deftest multiply-test
-  (is (= (* 1 2) (multiply 1 2))))
-
-(deftest multiply-test-2
-  (is (= (* 75 10) (multiply 10 75))))
+     [org.londonclojurians.landing-page :as landing-page]))
